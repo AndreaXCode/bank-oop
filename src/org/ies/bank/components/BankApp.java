f41370accf116f611a9af633308d60b15e88b430
Andrea <andrea@bases>
2025-01-16 11:04:33 +0100

    import quitado

--- src/org/ies/bank/components/BankApp.java
+++ src/org/ies/bank/components/BankApp.java
@@ -1,6 +1,5 @@
 package org.ies.bank.components;
 
-import org.ies.bank.model.Account;
 import org.ies.bank.model.Bank;
 import org.ies.bank.model.Customer;
 
@@ -37,22 +36,7 @@ public class BankApp {
 
             // Mostramos las opciones que tendrá nuestro menú
 
-            System.out.println("Elige una opción:");
-            System.out.println("1.Mostrar las cuentas del banco");
-            System.out.println("2.Mostrar datos de una cuenta");
-            System.out.println("3.Mostrar los datos de las cuentas de un cliente");
-            System.out.println("4.Ingresar dinero en cuenta");
-            System.out.println("5.Sacar dinero de una cuenta");
-            System.out.println("6.Mostrar el número de cuentas de un cliente");
-            System.out.println("7.Mostrar los datos del cliente al que pertenece la cuenta");
-            System.out.println("8.Contar cuentas de cliente");
-            System.out.println("9.Mostrar cliente de cuenta");
-            System.out.println("10.Realizar transferencia");
-            System.out.println("11.Salir");
-
-            //Leer la opcion
-            option = scanner.nextInt();
-            scanner.nextLine();
+            option = chooseOption();
 
             if (option == 1) {
 
@@ -63,67 +47,27 @@ public class BankApp {
 
             } else if (option == 2) {
 
-                //Mostrar datos de una cuenta
-                System.out.println("Introduce un IBAN:");
-                String iban = scanner.nextLine();
-
-
-                // Ejecuta el método showInfo que está dentro de bank
-                bank.showInfo(iban);
+                showInfo(bank);
 
 
             } else if (option == 3) {
 
-                // Mostrar los datos de las cuentas de un cliente
-
-                // Se pide un nif
-                System.out.println("Introduce un NIF:");
-                String nif = scanner.nextLine();
-
-
-                // Ejecuta el método showCountsClient que está dentro de bank
-                bank.showAccountsClient(nif);
+                showAccountsClient(bank);
 
 
             } else if (option == 4) {
 
-                //Ingresar dinero en cuenta
-
-                //Se pide un IBAN
-                System.out.println("Introduce un IBAN:");
-                String iban = scanner.nextLine();
-
-                //Se pide una cantidad de dinero
-                System.out.println("Introduce una cantidad de dinero");
-                int amount = scanner.nextInt();
-                scanner.nextLine();
-
-                bank.deposit(iban, amount);
+                deposit(bank);
 
 
             } else if (option == 5) {
 
-                System.out.println("Introduce un IBAN:");
-                String iban = scanner.nextLine();
-
-                System.out.println("Introduce la cantidad de dinero que quiere sacar:");
-                int amount = scanner.nextInt();
-                scanner.nextLine();
-
-                //Para sacar (restar)
-                bank.getIntoCount(iban, -amount);
-
+                getIntoCount(bank);
 
 
             } else if (option == 6) {
 
-                System.out.println("Introduce un NIF:");
-                String nif = scanner.nextLine();
-
-
-                int count = bank.countNumber(nif);
-                System.out.println(count);
-
+                countNumber(bank);
 
 
             } else if (option == 7) {
@@ -139,37 +83,15 @@ public class BankApp {
 
                 } else {
 
-                    System.out.println("Datos del cliente: " + customer);
+                    customer.showInfo();
 
                 }
 
 
+
+
             } else if (option == 8) {
 
-
-                //Hacer un método para contar cuentas de cliente
-                System.out.println("Introduce un nif:");
-                String nif = scanner.nextLine();
-
-                int account = bank.countAccounts(nif);
-
-                System.out.println(account);
-
-
-
-            } else if (option == 9) {
-
-                // Hacer método para mostrar cliente de cuenta
-                System.out.println("Introduce un iban:");
-                String iban = scanner.nextLine();
-
-                Customer customer = bank.findAccountCustomer(iban);
-                System.out.println("Cliente: " + customer);
-
-
-
-
-            } else if (option == 10) {
 
                 // Realizar transferencia
 
@@ -192,11 +114,7 @@ public class BankApp {
 
 
 
-
-
-
-
-            } else if (option == 11){
+            } else if (option == 9){
 
                 System.out.println("Saliendo del menu...");
             }
@@ -208,9 +126,86 @@ public class BankApp {
             }
 
 
-        } while (option != 11);
+        } while (option != 9);
 
 
+    }
+
+    private void countNumber(Bank bank) {
+        System.out.println("Introduce un NIF:");
+        String nif = scanner.nextLine();
+
+
+        int count = bank.countNumber(nif);
+        System.out.println(count);
+    }
+
+    private void getIntoCount(Bank bank) {
+        System.out.println("Introduce un IBAN:");
+        String iban = scanner.nextLine();
+
+        System.out.println("Introduce la cantidad de dinero que quiere sacar:");
+        int amount = scanner.nextInt();
+        scanner.nextLine();
+
+        //Para sacar (restar)
+        bank.getIntoCount(iban, -amount);
+    }
+
+    private void deposit(Bank bank) {
+        //Ingresar dinero en cuenta
+
+        //Se pide un IBAN
+        System.out.println("Introduce un IBAN:");
+        String iban = scanner.nextLine();
+
+        //Se pide una cantidad de dinero
+        System.out.println("Introduce una cantidad de dinero");
+        int amount = scanner.nextInt();
+        scanner.nextLine();
+
+        bank.deposit(iban, amount);  //invocar
+    }
+
+    private void showAccountsClient(Bank bank) {
+        // Mostrar los datos de las cuentas de un cliente
+
+        // Se pide un nif
+        System.out.println("Introduce un NIF:");
+        String nif = scanner.nextLine();
+
+
+        // Ejecuta el método showCountsClient que está dentro del objeto bank
+        bank.showAccountsClient(nif);
+    }
+
+    private void showInfo(Bank bank) {
+        //Mostrar datos de una cuenta
+        System.out.println("Introduce un IBAN:");
+        String iban = scanner.nextLine();
+
+
+        // Ejecuta el método showInfo que está dentro de bank
+        bank.showInfo(iban);
+    }
+
+    private int chooseOption() {
+        int option;
+        System.out.println("Elige una opción:");
+        System.out.println("1.Mostrar las cuentas del banco");
+        System.out.println("2.Mostrar datos de una cuenta");
+        System.out.println("3.Mostrar los datos de las cuentas de un cliente");
+        System.out.println("4.Ingresar dinero en cuenta");
+        System.out.println("5.Sacar dinero de una cuenta");
+        System.out.println("6.Mostrar el número de cuentas de un cliente");
+        System.out.println("7.Mostrar los datos del cliente al que pertenece la cuenta");
+        System.out.println("8.Realizar transferencia");
+        System.out.println("9.Salir");
+
+        //Leer la opcion
+        option = scanner.nextInt();
+        scanner.nextLine();
+        return option;
     }
 
 }
